39bc756b22c7fb2eefc3cd1792e6fa6e9a11d036
Karissa <k@karissademi.com>
2015-02-02 12:29:56 -0500

    Create Business CPT

--- functions.php
+++ functions.php
@@ -22,3 +22,70 @@ function dynamic_section($sections) {
     return $sections;
 }
 */
+
+/**
+ * Contains :
+ * custom post types
+    * Business Type - business_page
+
+ *
+ * @link https://github.com/kdemi/business-services-child-theme
+ *
+ * @package business-services-child-theme
+ */
+/**
+ *  Create Custom Post Types
+ *
+ * Additional custom post types can be defined here
+ * http://codex.wordpress.org/Post_Types
+ *
+ * @link https://github.com/kdemi/business-services-child-theme
+ *
+ *
+ */
+
+if (!class_exists('BusinessServicesCustomPostTypes')){
+    class BusinessServicesCustomPostTypes{
+        function create_business_post_type() {
+          register_post_type( 'business_page',
+            array(
+                'labels' => array(
+                    'name' => __( 'Business Type' ),
+                    'singular_name' => __( 'Business Type' ),
+                    'add_new'   => __('Add Business Type Page'),
+                    'all_items'   => __('All Business Type Pages'),
+                    'add_new_item' => __('Add Business Type Page'),
+                    'edit_item'   => __('Edit Business Type Page'),
+                    'view_item'   => __('View Business Type Page'),
+                    'search_items'   => __('Search Business Type Pages'),
+                    'not_found'   => __('Business Type Page Not Found'),
+                    'not_found_in_trash'   => __('Business Type Page not found in trash'),
+              ),
+                'taxonomies' => array('category'),
+                'public' => true,
+                'has_archive' => true,
+                'menu_position' => 5,
+                'menu_icon' => 'dashicons-groups',
+                'hierarchical' => true,
+                'rewrite' => array(
+                    'slug' => 'business',
+                ),
+            )
+          );
+        }
+
+    }//end class
+
+}
+
+
+if (class_exists("BusinessServicesCustomPostTypes")){
+    $custom_post_types = new BusinessServicesCustomPostTypes();
+}
+
+if (isset($custom_post_types)){
+    //actions
+    add_action( 'init', array($custom_post_types, 'create_business_post_type'));
+
+    register_activation_hook( __FILE__, array($custom_post_types, 'rewrite_flush') );
+}
