d156b770738ee51285cfd7b7913afe090cec8649
Karissa <k@karissademi.com>
2015-03-10 16:14:00 -0400

    business type page functionality

--- single-business_page.php
+++ single-business_page.php
@@ -33,7 +33,7 @@ get_template_part('template-parts/banner');
                               <?php
                                 echo '<h2>You Must Have</h2>' ;
                                 $required_docs = get_field('required');
-                              //  var_dump($required_docs);
+
                                   if($required_docs)  {
                                     foreach($required_docs as $required_doc){
                                       $content_types =  wp_get_post_terms( $required_doc->ID, 'content_type' );
@@ -61,21 +61,26 @@ get_template_part('template-parts/banner');
                                 <?php
                                   echo '<h2>You Might Need</h2>' ;
                                   $maybe_docs = get_field('might_need');
-                                    if($maybe_docs)  {
-                                      echo '<ul>';
+                                    if($maybe_docs) {
 
                                       foreach($maybe_docs as $maybe_doc){
-                                        echo '<li>';
+                                        $m_content_types =  wp_get_post_terms( $maybe_doc->ID, 'content_type' );
+
+                                        if (! $m_content_types == ''){
+                                          $m_content_types =  wp_get_post_terms( $maybe_doc->ID, 'content_type' );
+                                          echo '<div ';
+                                          foreach($m_content_types as $m_content_type){
+                                            echo 'class=' . $m_content_type->slug . '>';
+                                            }
+                                          }else {
+                                            echo '<div class="an-overview">';
+                                          }
+
                                         echo '<a href="' . $maybe_doc->guid .'">'  . $maybe_doc->post_title . '</a>';
 
-                                        $content_types =  wp_get_post_terms( $maybe_doc->ID, 'content_type' );
-                                        foreach($content_types as $content_type){
-                                          echo '<span class="label label-primary">' . $content_type->name . '</span>';
-                                        }
-                                        echo '<p>' .  get_post($maybe_doc->ID)->post_excerpt . '</p>';
-                                        echo '</li>';
+                                        echo '<p class="small">' .  get_post($maybe_doc->ID)->post_excerpt . '</p>';
+                                        echo '</div>';
                                       }
-                                      echo '</ul>';
                                     }
                                   ?>
                                 </div><!--#might-need -->
@@ -92,37 +97,19 @@ get_template_part('template-parts/banner');
 </div>
 <?php get_footer(); ?>
 
-    <?php
+<?php
+    $taxonomies = array('content_type');
+    $args = array(
+        'orderby'           => 'name',
+        'order'             => 'ASC',
+        'hide_empty'        => true,
+        'hierarchical'      => true,
+    );
 
-        $taxonomies = array(
-            'content_type'
-        );
-
-        $args = array(
-            'orderby'           => 'name',
-            'order'             => 'ASC',
-            'hide_empty'        => true,
-            'exclude'           => array(),
-            'exclude_tree'      => array(),
-            'include'           => array(),
-            'number'            => '',
-            'fields'            => 'all',
-            'slug'              => '',
-            'parent'            => '',
-            'hierarchical'      => true,
-            'child_of'          => 0,
-            'get'               => '',
-            'name__like'        => '',
-            'description__like' => '',
-            'pad_counts'        => false,
-            'offset'            => '',
-            'search'            => '',
-            'cache_domain'      => 'core'
-        );
-
-        $terms = get_terms($taxonomies, $args);
-        $tax_slugs = array();
-          foreach($terms as $term) {
-            array_push($tax_slugs, $term->slug);
-          }
-?><div id='dom-target'><?php foreach($tax_slugs as $key => $value){echo  htmlspecialchars($value . " ") ;}?></div>
+    $terms = get_terms($taxonomies, $args);
+    $tax_slugs = array();
+      foreach($terms as $term) {
+        array_push($tax_slugs, $term->slug);
+      }
+?>
+<div id="dom-target"><?php foreach($tax_slugs as $key => $value){echo  htmlspecialchars($value . " ") ;}?></div>
