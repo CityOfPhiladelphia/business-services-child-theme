b1929c227649374f0acb13cb08678f4055e6e591
Karissa <k@karissademi.com>
2015-03-12 15:20:46 -0400

    modify to use Mega Project

--- functions.php
+++ functions.php
@@ -1,27 +1,4 @@
 <?php
-/**
-
-Custom function for filtering the sections array. Good for child themes to override or add to the sections.
-
-NOTE: the defined constants for URLs, and directories will NOT be available at this point in a child theme,
-so you must use get_template_directory_uri() if you want to use any of the built in icons
-**/
-
-// REMOVE COMMENTS AROUND THIS FUNCTION IF YOU WANT TO PLAY WITH THEME OPTIONS
-/*
-function dynamic_section($sections) {
-    //$sections = array();
-    $sections[] = array(
-        'title' => __('Section via hook', 'framework'),
-        'desc' => __('<p class="description">This is a section created by adding a filter to the sections array. Can be used by child themes to add/remove sections from the options.</p>', 'framework'),
-        'icon' => 'el-icon-paper-clip',
-        // Leave this as a blank section, no options just some intro text set above.
-        'fields' => array()
-    );
-
-    return $sections;
-}
-*/
 
 /**
  * Contains :
@@ -43,6 +20,25 @@ function dynamic_section($sections) {
  *
  *
  */
+
+ /*-----------------------------------------------------------------------------------*/
+ /*	enqueue styles
+ /*-----------------------------------------------------------------------------------*/
+
+
+ add_action( 'init', 'theme_enqueue_styles' );
+ function theme_enqueue_styles() {
+ if(!is_admin()){
+     wp_dequeue_style('pkt-style');
+     wp_enqueue_style( 'pkt-style', get_template_directory_uri() . '/style.css');
+     wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('pkt-style'));
+ }
+ }
+
+ /*-----------------------------------------------------------------------------------*/
+ /*	Custom Post Types
+ /*-----------------------------------------------------------------------------------*/
+
 
 if (!class_exists('BusinessServicesCustomPostTypes')){
     class BusinessServicesCustomPostTypes{
@@ -141,10 +137,10 @@ if (isset($custom_taxonomy)){
 
 function remove_medical_press_theme_features() {
    // remove Movie Custom Post Type
-   remove_action( 'init', 'create_doctor_post_type' );
-   remove_action( 'init', 'create_gallery_post_type' );
-   remove_action( 'init', 'create_testimonial_post_type' );
-   remove_action( 'init', 'create_service_post_type' );
+//   remove_action( 'init', 'create_doctor_post_type' );
+//   remove_action( 'init', 'create_gallery_post_type' );
+//   remove_action( 'init', 'create_testimonial_post_type' );
+//   remove_action( 'init', 'create_service_post_type' );
 
    //remove theme support for post formats
    remove_theme_support('post-formats');
@@ -162,113 +158,6 @@ function unregister_taxonomy(){
     register_taxonomy('post_tag', array());
 }
 add_action('init', 'unregister_taxonomy');
-
-
-/*-----------------------------------------------------------------------------------*/
-/*	Override Parent Theme Breadcrumb
-/*-----------------------------------------------------------------------------------*/
-
-if (!function_exists('theme_breadcrumb')) {
-    function theme_breadcrumb()
-    {
-
-        echo '<ul class="breadcrumb clearfix">';
-
-        /* For all pages other than front page */
-        if (!is_front_page()) {
-            echo '<li>';
-            echo '<a href="' . home_url() . '">' . get_bloginfo('name') . '</a>';
-            echo '<span class="divider"></span></li>';
-        }
-
-        /* For index.php OR blog posts page */
-        if (is_home()) {
-            $page_for_posts = get_option('page_for_posts');
-            if ($page_for_posts) {
-                $blog = get_post($page_for_posts);
-                echo '<li>';
-                echo $blog->post_title;
-                echo '</li>';
-            } else {
-                echo '<li>';
-                _e('Blog', 'framework');
-                echo '<li>';
-            }
-        }
-
-        if (is_category() || is_singular('post')) {
-            $category = get_the_category();
-            $ID = $category[0]->cat_ID;
-            echo '<li>';
-            echo get_category_parents($ID, TRUE, ' <span class="divider"></span></li><li>', FALSE);
-        }
-
-        if (is_tax('gallery-item-type') || is_tax('department')) {
-            $current_term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
-            if (!empty($current_term->name)) {
-                echo '<li class="active">';
-                echo $current_term->name;
-                echo '</li>';
-            }
-        }
-
-        if (is_singular('post') || is_singular('business_page') || is_singular('service') || is_singular('gallery-item') || is_page()) {
-            global $post;
-            $parent_id = $post->post_parent;
-            if(  is_page() && $parent_id ){
-                $parents = array();
-                while ( $parent_id ) {
-                    $parents[] = $parent_id;
-                    $page = get_post( $parent_id );
-                    $parent_id = $page->post_parent;
-                }
-                $parents_count = count( $parents );
-                for( $i = $parents_count; $i > 0; ){
-                    $parent_id = $parents[--$i];
-                    echo '<li>';
-                        echo '<a href="' . get_the_permalink( $parent_id ) . '">' ;
-                        echo get_the_title( $parent_id );
-                        echo '</a>';
-                        echo '<span class="divider"></span>';
-                    echo '</li>';
-                }
-            }
-
-            echo '<li class="active">';
-            the_title();
-            echo '</li>';
-        }
-
-        if (is_tag()) {
-            echo '<li>';
-            _e('Tag: ', 'framework');
-            echo single_tag_title('', FALSE);
-            echo '</li>';
-        }
-
-        if (is_404()) {
-            echo '<li>';
-            _e('404 - Page not Found', 'framework');
-            echo '</li>';
-
-        }
-
-        if (is_search()) {
-            echo '<li>';
-            _e('Search', 'framework');
-            echo '</li>';
-        }
-
-        if (is_year()) {
-            echo '</li>';
-            echo get_the_time('Y');
-            echo '</li>';
-        }
-
-        echo "</ul>";
-
-    }
-}
 
 
 /*-----------------------------------------------------------------------------------*/
--- single-business_page.php
+++ single-business_page.php
@@ -3,99 +3,91 @@
 get_header();
 get_template_part('template-parts/banner');
 ?>
-<div class=" page-top clearfix">
-    <div class="container">
-        <div class="row">
-            <div class="<?php bc_all('12'); ?>">
-                <h1 class="entry-title"><?php the_title(); ?></h1>
-                <nav class="bread-crumb">
-                    <?php theme_breadcrumb(); ?>
-                </nav>
-            </div>
-        </div>
-    </div>
-</div>
+<?php get_header(); ?>
 
-<div class="business-page clearfix">
-    <div class="container">
-        <div class="row">
-            <div class="<?php bc_all('12'); ?>">
-                <?php
-                if (have_posts()):
-                    while (have_posts()):
-                        the_post();
-                        ?>
-                        <article id="post-<?php the_ID(); ?>" <?php post_class(' clearfix'); ?>>
-                            <div class="entry-content">
-                                <?php the_content(); ?>
+  <div class="gdlr-content">
 
-                              <div id="must-have">
-                              <?php
-                                echo '<h2>You Must Have</h2>' ;
-                                $required_docs = get_field('required');
+    <!-- Above Sidebar Section-->
+    <?php global $gdlr_post_option, $above_sidebar_content, $with_sidebar_content, $below_sidebar_content; ?>
+    <?php if(!empty($above_sidebar_content)){ ?>
+      <div class="above-sidebar-wrapper"><?php gdlr_print_page_builder($above_sidebar_content); ?></div>
+    <?php } ?>
 
-                                  if($required_docs)  {
-                                    foreach($required_docs as $required_doc){
-                                      $content_types =  wp_get_post_terms( $required_doc->ID, 'content_type' );
-
-                                      if (! $content_types == ''){
-                                        $content_types =  wp_get_post_terms( $required_doc->ID, 'content_type' );
-                                        echo '<div ';
-                                        foreach($content_types as $content_type){
-                                          echo 'class=' . $content_type->slug . '>';
-                                          }
-                                        }else {
-                                          echo '<div class="an-overview">';
-                                        }
-
-                                      echo '<a href="' . $required_doc->guid .'">'  . $required_doc->post_title . '</a>';
-
-                                      //pass the post ID to get_post, then extract the excerpt. BOOYAH
-                                      echo  '<span>' . get_post($required_doc->ID)->post_excerpt . '</span>';
-                                      echo '</div>';
-                                    }
-                                  }
-                                  ?>
-                                </div><!--#must-have-->
-                                  <div id="might-need">
-                                <?php
-                                  echo '<h2>You Might Need</h2>' ;
-                                  $maybe_docs = get_field('might_need');
-                                    if($maybe_docs) {
-
-                                      foreach($maybe_docs as $maybe_doc){
-                                        $m_content_types =  wp_get_post_terms( $maybe_doc->ID, 'content_type' );
-
-                                        if (! $m_content_types == ''){
-                                          $m_content_types =  wp_get_post_terms( $maybe_doc->ID, 'content_type' );
-                                          echo '<div ';
-                                          foreach($m_content_types as $m_content_type){
-                                            echo 'class=' . $m_content_type->slug . '>';
-                                            }
-                                          }else {
-                                            echo '<div class="an-overview">';
-                                          }
-
-                                        echo '<a href="' . $maybe_doc->guid .'">'  . $maybe_doc->post_title . '</a>';
-
-                                        echo '<span>' .  get_post($maybe_doc->ID)->post_excerpt . '</span>';
-                                        echo '</div>';
-                                      }
-                                    }
-                                  ?>
-                                </div><!--#might-need -->
-                            </div>
-                        </article>
-                    <?php
-                    endwhile;
-                endif;
+              <?php
+                  get_template_part('single/content', 'page');
                 ?>
-            </div>
-        </div>
+            <div class="container">
+              <div class="gdlr-item gdlr-main-content">
+                <div id="must-have">
+            <?php
+              echo '<h2>You Must Have</h2>' ;
+              $required_docs = get_field('required');
 
-    </div>
-</div>
+                if($required_docs)  {
+                  foreach($required_docs as $required_doc){
+                    $content_types =  wp_get_post_terms( $required_doc->ID, 'content_type' );
+
+                    if (! $content_types == ''){
+                      $content_types =  wp_get_post_terms( $required_doc->ID, 'content_type' );
+                      echo '<div ';
+                      foreach($content_types as $content_type){
+                        echo 'class=' . $content_type->slug . '>';
+                        }
+                      }else {
+                        echo '<div class="an-overview">';
+                      }
+
+                    echo '<a href="' . $required_doc->guid .'">'  . $required_doc->post_title . '</a>';
+
+                    //pass the post ID to get_post, then extract the excerpt. BOOYAH
+                    echo  '<span>' . get_post($required_doc->ID)->post_excerpt . '</span>';
+                    echo '</div>';
+                  }
+                }
+                ?>
+              </div><!--#must-have-->
+                <div id="might-need">
+              <?php
+                echo '<h2>You Might Need</h2>' ;
+                $maybe_docs = get_field('might_need');
+                  if($maybe_docs) {
+
+                    foreach($maybe_docs as $maybe_doc){
+                      $m_content_types =  wp_get_post_terms( $maybe_doc->ID, 'content_type' );
+
+                      if (! $m_content_types == ''){
+                        $m_content_types =  wp_get_post_terms( $maybe_doc->ID, 'content_type' );
+                        echo '<div ';
+                        foreach($m_content_types as $m_content_type){
+                          echo 'class=' . $m_content_type->slug . '>';
+                          }
+                        }else {
+                          echo '<div class="an-overview">';
+                        }
+
+                      echo '<a href="' . $maybe_doc->guid .'">'  . $maybe_doc->post_title . '</a>';
+
+                      echo '<span>' .  get_post($maybe_doc->ID)->post_excerpt . '</span>';
+                      echo '</div>';
+                    }
+                  }
+
+                ?>
+
+              </div><!--#might-need -->
+            </div>
+          </div>
+
+
+    <!-- Below Sidebar Section-->
+    <?php if(!empty($below_sidebar_content)){ ?>
+      <div class="below-sidebar-wrapper"><?php gdlr_print_page_builder($below_sidebar_content); ?></div>
+    <?php } ?>
+
+  </div><!-- gdlr-content -->
 <?php get_footer(); ?>
+
+
 
 <?php
     $taxonomies = array('content_type');
