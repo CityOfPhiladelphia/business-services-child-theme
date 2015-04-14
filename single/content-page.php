81fe97b8eb58a02a26eea3c1c40f9beb6738b074
Karissa <k@karissademi.com>
2015-04-14 15:00:07 -0400

    menu chooser on pages

---
+++ single/content-page.php
@@ -0,0 +1,29 @@
+<?php
+	while ( have_posts() ){ the_post();
+		$content = gdlr_content_filter(get_the_content(), true);
+		if(!empty($content)){
+			?>
+			<div class="main-content-container container gdlr-item-start-content">
+				<div class="gdlr-item gdlr-main-content">
+					<?php 
+          echo $content;
+            $menus = get_field('page_menu');
+  				     if( $menus ):
+  						?>	<ul>
+  							<?php foreach( $menus as $menu ): ?>
+  								<li>
+  									<a href="<?php echo get_permalink( $menu->ID ); ?>">
+  										<?php echo get_the_title( $menu->ID ); ?>
+  									</a>
+  								</li>
+  							<?php endforeach; ?>
+  							</ul>
+  						<?php endif; ?>
+
+					<div class="clear"></div>
+				</div>
+			</div>
+			<?php
+		}
+	}
+?>
