3f7846d856d615a527d74a28cde43a4485954275
Karissa <k@karissademi.com>
2015-04-29 16:54:43 -0400

    remove return false

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,6 @@ jQuery(document).ready(function($) {
 
   $(".home .gdlr-box-with-icon-item").click(function() {
     window.location = $('this').find("a").attr("href");
-    return false;
+    //return false;
   });
 });
