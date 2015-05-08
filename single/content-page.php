003d24b788eabf668a5c18dd9be5404adf275c1b
Karissa <k@karissademi.com>
2015-05-08 11:24:34 -0400

    standardize content page

--- single/content-page.php
+++ single/content-page.php
@@ -12,14 +12,17 @@
 					<?php
 							$menus = get_field('page_menu');
 							if( $menus ):
-								foreach( $menus as $menu ): ?>
+								foreach( $menus as $menu ):
+									$current_ID = $menu->ID;
+									 ?>
   								<div class="list nine columns">
-  									<a href="<?php echo get_permalink( $menu->ID ); ?>">
-  										<?php echo get_the_title( $menu->ID );
+  									<a href="<?php echo get_permalink( 	$current_ID ); ?>">
+  										<?php echo get_the_title( 	$current_ID );
                     ?>
   									</a>
-                    <?php echo  '<p>' . get_post($menu->ID)->post_excerpt . '</p>';?>
+                    <?php echo  '<p>' . get_post(	$current_ID )->post_excerpt . '</p>';?>
   								</div>
+									<?php echo '<div class="more one columns">' . '<a href="' . 	get_post($current_ID)->guid .'" class="button full"><i class="fa fa-arrow-circle-right"></i>' . 'Read More' . '</a></div>'; ?>
 	  							<?php endforeach; ?>
 	  						<?php endif; ?>
 						</div>
