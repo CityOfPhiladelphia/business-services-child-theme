02e3fe660cda4333408b7db34cd9bf383b1935b9
Karissa Demi <k@karissademi.com>
2015-07-02 09:50:34 -0400

    remove content-feedback

--- templates/content-feedback.php
+++
@@ -1,6 +0,0 @@
-<div class="feedback container">
-  <div class="eight columns"><p>Welcome to the new Business Services Website</p></div>
-  <div class="four columns">
-      <a class="feedback button" href="https://docs.google.com/forms/d/12wcsvC97oa2U50BWQyb_rsqaPPH4GoEd9blT859MpIc/viewform?entry.893934780&entry.526820711=<?php echo $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] ?>" target="_blank"><i class="fa fa-comments"></i> Provide Feedback <span class="accessible"> Opens in new window</span></a></div>
-</div>
-<div class="clear"></div>
