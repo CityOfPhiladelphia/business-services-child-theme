3e4801a3deaea02e3bfcc981838d81dbd23c2e6a
Karissa Demi <k@karissademi.com>
2015-07-09 16:28:39 -0400

    update 404

--- 404.php
+++ 404.php
@@ -7,12 +7,9 @@ get_header(); ?>
 
 	<div class="page-not-found-container container">
 		<div class="gdlr-item page-not-found-item">
-			<div class="page-not-found-block" >
-				<div class="page-not-found-title">
-					<?php _e('Error 404', 'gdlr_translate'); ?>
-				</div>
+			<div class="page-not-found-block">
 				<div class="page-not-found-caption">
-					<?php _e('Sorry, we couldn\'t find the page you\'re looking for.', 'gdlr_translate'); ?>
+					<?php echo 'The page you were looking for may have been moved. <br> Try a keyword search or <a href="/index.php">go to the homepage</a>.' ?>
 				</div>
 				<div class="page-not-found-search">
 					<?php get_search_form(); ?>
